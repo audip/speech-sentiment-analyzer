171bc85fdf9542b5bd0eca7e14f033ebcbd8de05
Saurabh Patil <saurabh.s.patil8989@gmail.com>
2016-11-13 13:14:55 -0600

    no message

--- view.py
+++ view.py
@@ -8,23 +8,22 @@ app = Flask(__name__)
 def home():
     tot_pos, tot_neg = 1, 1
     hateDocuments = []
-    lst = []
     if request.method == 'POST':
         searchTerm = request.form['searchBox']
-        #
-        conn = MongoClient('mongodb://audip:audip@ds151917.mlab.com:51917/speech-db')
+
+        conn = MongoClient('mongodb://audip:audip@ds151917.mlab.com:51917')
         db = conn['speech-db']
         collection = db['hate_speech']
         hateDocuments = collection.find({"hate": "true"})
         documents = collection.find({"sentiment": {"$ne": "neutral"}})
+
         for doc in documents:
-            lst.append(doc)
             if doc["sentiment"] == 'Positive':
                 tot_pos += 1
             else:
                 tot_neg += 1
-    print(tot_pos, tot_neg)
-    return render_template('index.html', hateTweets=lst, positive=(tot_pos/(tot_neg+tot_pos)), negative=(tot_neg/(tot_neg+tot_pos)))
+
+    return render_template('index.html', hateTweets=hateDocuments, positive=(tot_pos/(tot_neg+tot_pos)), negative=(tot_neg/(tot_neg+tot_pos)))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
